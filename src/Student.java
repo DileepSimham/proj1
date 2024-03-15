8846eaea9f35e4125a73dc6a6da689660c34621c
Dileep Simham <dileep.simham483@gmail.com>
2024-03-15 18:04:55 +0530

    main method added

--- src/Student.java
+++ src/Student.java
@@ -4,6 +4,10 @@ public class Student {
     private String name;
     private String college;
     private String country;
+
+      public static void main(String[] args) {
+        System.out.println("Student class ok");
+    }
     
 
 
