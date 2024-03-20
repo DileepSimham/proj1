0e7afedee8d4ebb698f349c7dff4244d6316ac36
Dileep Simham <dileep.simham483@gmail.com>
2024-03-20 20:39:48 +0530

    converted to StudentEnt

--- src/StudentEnt.java
+++ src/StudentEnt.java
@@ -1,19 +1,18 @@
-public class StudentEntity {
+public class StudentEnt {
 
     private int sId;
     private String name;
     private String college;
     private String country;
 
-
-
-
-
-      public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("Student class ok");
 
     }
-    
+
+    public static void main(String[] args) {
+
+    }
 
 
 }
